c71ef7288f1062cb6251260ae7b1502276ec9a44
Christopher Brown <io@henrian.com>
2013-11-16 12:41:46 -0600

    Smooth check-in

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 'use strict'; /*jslint es5: true, node: true, indent: 2 */
 var express = require('express');
+var async = require('async');
+var eyes = require('eyes');
 var moment = require('moment');
 var winston = require('winston');
 var hbs = require('hbs');
 
 var Room = require('./lib/models').Room;
-var logger = new winston.Logger();
+var logger = new winston.Logger({level: 'debug'});
 
 var app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 app.engine('html', hbs.__express);
 
-hbs.registerHelper('time', function(date) {
+hbs.registerHelper('format_hmma', function(date) {
   return moment(date).format('h:mm a');
+});
+
+hbs.registerHelper('from_now', function(date) {
+  return moment(date).fromNow();
 });
 
 
 app.post('/sensor/:name', function(req, res) {
-  console.log('Looking for room: %s', req.params.name);
+  // console.log('Looking for room: %s', req.params.name);
   Room.findById(req.params.name, function(err, room) {
     if (err || room === null) {
       console.error('Creating new Room: %s', room);
@@ -37,15 +43,54 @@ app.post('/sensor/:name', function(req, res) {
 
 app.get('/smooth', function(req, res) {
   var two_hours_ago = moment().subtract(moment.duration(2, 'hours'));
+  // span is the raw duration (milliseconds) between now and two hours ago
+  var span = moment().diff(two_hours_ago);
+
+  var slot_length = moment.duration(15, 'minutes');
+
+  var slots = [];
+  var quarter = moment().get('minutes') / 15 | 0;
+  var cursor = moment().set('minutes', quarter * 15).set('seconds', 0);
+  while (cursor > two_hours_ago) {
+    var time = cursor.clone();
+    slots.unshift({
+      time: time.toDate(),
+      offset: 100 * moment().diff(time) / span,
+    });
+    cursor.subtract(slot_length);
+  }
+
   Room.find(function(err, rooms) {
     rooms = rooms.map(function(room) {
-      var recent_times = room.times.filter(function(time) {
+      var times = room.times.filter(function(time) {
         return time > two_hours_ago;
+      }).map(function(time) {
+        return {
+          time: time,
+          offset: 100 * moment().diff(time) / span,
+        };
       });
-      return {name: room.name, description: room.description, times: recent_times};
+      return {
+        name: room._id,
+        description: room.description,
+        last: room.times[room.times.length - 1],
+        times: times,
+      };
     });
-    var context = {rooms: rooms, two_hours_ago: two_hours_ago, now: new Date()};
+    var context = {
+      rooms: rooms,
+      slots: slots,
+      // two_hours_ago: two_hours_ago,
+      now: new Date(),
+    };
+    // eyes.inspect(context, 'smooth context');
     res.render('smooth', context);
+  });
+});
+
+app.get('/populate', function(req, res) {
+  //
+  Room.find(function(err, rooms) {
   });
 });
 
